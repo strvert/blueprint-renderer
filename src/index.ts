a4518748db6ffd8e92c5abe2f3ef4f1763128a30
strvworks <strv13570@gmail.com>
2021-12-05 02:24:13 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { createNode } from "./classes/nodes/NodeBase";
 import { buildBasicNode } from "./classes/nodes/BasicNodeBuilder";
 import { Graph } from "./classes/Graph";
-
-export { Graph, buildBasicNode, createNode };
+export { Graph, buildBasicNode };
